09768b1d678b5d0a27d55ff377d9529123bbff4b
Scott G. Daniel <scottdaniel25@gmail.com>
2016-04-21 16:51:38 -0700

    stupid git revert

--- scripts/workers/make-fastaDB.sh
+++ scripts/workers/make-fastaDB.sh
@@ -6,12 +6,20 @@
 #PBS -l jobtype=cluster_only
 #PBS -l select=1:ncpus=2:mem=3gb:pcmem=2gb
 #PBS -l pvmem=6gb
-#PBS -l walltime=00:60:00
-#PBS -l cput=00:60:00
+#PBS -l walltime=00:30:00
+#PBS -l cput=00:30:00
 #PBS -M scottdaniel@email.arizona.edu
 #PBS -m bea
 
 cd $PBS_O_WORKDIR
+CONFIG="$PRJ_DIR/scripts/config.sh"
+
+if [ -e $CONFIG ]; then
+    . "$CONFIG"
+else
+    echo MIssing config \"$CONFIG\"
+    exit 12385
+fi
 
 COMMON="$WORKER_DIR/common.sh"
 
@@ -21,6 +29,12 @@ else
   echo Missing common \"$COMMON\"
   exit 1
 fi
+
+PROG=`basename $0 ".sh"`
+#Just going to put stdout and stderr together into stdout
+STDOUT_DIR="$CWD/out/$PROG"
+
+init_dir "$STDOUT_DIR"
 
 TMP_FILES=$(mktemp)
 
@@ -32,7 +46,7 @@ echo Found \"$NUM_FILES\" files to process
 
 while read FASTA; do
 
-    FILE=$PATRIC_GENOMES/$FASTA
+    FILE=$SPLIT_FA_DIR/$FASTA
     
     perl $WORKER_DIR/make-fastaDB.pl $FILE
 
